042001b0050f3d9af887d2f1e5dd224bfccf8522
SinnahSaint <sinnah.saint@gmail.com>
2022-01-28 17:20:08 -0500

    Actually links through activities_tasks now

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,3 +1,4 @@
 class Activity < ApplicationRecord
-  has_many :tasks, through :activities_tasks
+  has_many :activities_tasks
+  has_many :tasks, through: :activities_tasks
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,5 @@
 class Task < ApplicationRecord
   belongs_to :user, optional: true
-  has_many :activities, through :activities_tasks
+  has_many :activities_tasks 
+  has_many :activities, through: :activities_tasks
 end
